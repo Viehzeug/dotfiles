942712a9faf368dc06ffa2ff03df4d9e84012086
Marc Fischer <spam@totallyawesome.at>
2019-05-29 22:10:38 +0200

    added emacs to setup script

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ ln -s  $SRC_DIR"/zsh/.zlogin" $HOME"/.zlogin"
 ln -s  $SRC_DIR"/zsh/.zshrc" $HOME"/.zshrc"
 ln -s  $SRC_DIR"/zsh/.zprofile" $HOME"/.zprofile"
 ln -s  $SRC_DIR"/.profile" $HOME"/.profile"
-
+ln -s  $SRC_DIR"/emacs" $HOME"/.emacs.d"
 
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.z.sh
